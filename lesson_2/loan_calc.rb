0256bc129f8e868bfddee74f9902b2571a2caf88
Matt Clark <clark.matthewr@gmail.com>
2019-01-24 16:39:59 -0600

    Adding loan calculator and rock, paper, scissors game

---
+++ lesson_2/loan_calc.rb
@@ -0,0 +1,48 @@
+def invalid
+  puts "Invalid input. Please enter a positive number."
+  puts ""
+end
+
+# take in loan amount, APR, and duration from user
+amount = 0
+loop do
+  puts "What is your total loan amount?"
+  print "Dollars: "
+  amount = gets.chomp.to_i  # loan amount
+  break if amount > 0
+  invalid
+end
+puts ""
+
+apr = 0
+loop do
+  puts "What is the APR for your loan?"
+  puts "(eg. for 5% enter '5', for 2.5% enter '2.5')"
+  print "Percentage: "
+  apr = gets.chomp  # APR
+  break if apr.to_i > 0
+  invalid
+end
+puts ""
+
+apr = apr.to_f / 100
+
+dur_years = 0
+loop do
+  puts "What is the duration of your loan?"
+  print "Years: "
+  dur_years = gets.chomp.to_i  # duration of loan in years
+  break if dur_years > 0
+  invalid
+end
+puts ""
+
+# calculate monthly interest rate and duration in months
+mpr = apr / 12
+dur_months = dur_years * 12
+
+# give monthly payment amount to pay off loan in loan duration
+payments = amount * (mpr / (1 - (1 + mpr)**(-dur_months)))
+interest = (payments * dur_months) - amount
+puts "For a #{dur_years} year loan of $#{amount} and an APR of #{apr * 100}%, you will pay $#{payments.round(2)} per month."
+puts "You'll pay a total of $#{interest.round(2)} in interest!"
---
+++ lesson_2/rock_paper_scissors.rb
@@ -0,0 +1,50 @@
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def win?(first, second)
+  (first == 1 && second == 2) ||
+    (first == 2 && second == 3) ||
+    (first == 3 && second == 1)
+end
+
+def display_results(user, comp)
+  if win?(user, comp)
+    "Computer wins!"
+  elsif win?(comp, user)
+    "Player wins!"
+  else
+    "Draw."
+  end
+end
+
+def computer_choice
+  rand(1..3)
+end
+
+prompt "Welcome to Rock, Paper, Scissors!"
+
+choices = ['rock', 'paper', 'scissors']
+
+loop do
+  user = 0
+  loop do
+    prompt "Which would you like to select? Enter 1, 2, or 3 to choose:"
+    prompt "  1. Rock"
+    prompt "  2. Paper"
+    prompt "  3. Scissors"
+    user = gets.chomp.to_i
+    break if user > 0 && user < 4
+    puts "Invalid input. You must type either '1', '2', or '3'."
+  end
+
+  comp = computer_choice
+  prompt "Your choice is #{choices[user-1]}."
+  prompt "Computer's choice is #{choices[comp-1]}."
+
+  prompt(display_results(user, comp))
+
+  puts ""
+  prompt("Play again?")
+  break unless gets.chomp.downcase.start_with?('y')
+end
